6e897f8c5daccf89dbdb8907a3b48ac5519ef68b
grizzlysh <dev.husseinfaisol@gmail.com>
2021-11-25 22:32:32 +0700

    add sign-in page with sign-in component, form-input component, and custom button component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import MenuItem from './components/menu-item/menu-item.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route exact path='/'element={<HomePage />} />
           <Route path='/:menuId'element={<MenuItem />} />
           <Route path='/shop'element={<ShopPage />} />
+          <Route path='/signin'element={<SignInAndSignUpPage />} />
         </Routes>
     </div>
   );
