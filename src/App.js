73ff8a74523c0d1fcee5dae2d3ea0dd9442b8051
grizzlysh <dev.husseinfaisol@gmail.com>
2021-12-02 23:55:28 +0700

    added redux, implemented userReducer and userActions to header and app compnents

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './App.css';
 import MenuItem from './components/menu-item/menu-item.component';
@@ -10,20 +11,14 @@ import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { onSnapshot } from 'firebase/firestore';
+import { setCurrentUser } from './redux/user/user.actions';
 
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-  
-    this.state = {
-      currentUser: null
-    }
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount(){
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
       
       if (userAuth) {
@@ -31,16 +26,14 @@ class App extends Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         onSnapshot( userRef, (snapShot) => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
         });
       }
       else{
-        this.setState({ currentUser: userAuth });
+        setCurrentUser(userAuth);
       }
       
       // console.log(this.currentUser);
@@ -56,7 +49,7 @@ class App extends Component {
   render() {
     return (
       <div>
-          <Header currentUser={ this.state.currentUser }/>
+          <Header />
           <Routes>
             <Route exact path='/'element={<HomePage />} />
             <Route path='/:menuId'element={<MenuItem />} />
@@ -68,4 +61,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils'
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss'
@@ -32,4 +33,9 @@ const Header = ({ currentUser }) => {
     )
 }
 
-export default Header;
+// state = root reducer
+const mapStateToProps = state => ({
+    currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
